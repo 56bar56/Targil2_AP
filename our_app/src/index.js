5160c81e603c2f9646e5225b7107ef344a4fbe41
ofekBinder <ofekBinder@gmail.com>
2023-05-19 18:38:17 +0300

    index

--- our_app/src/index.js
+++ our_app/src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import LoginScreen from './LoginScreen/LoginScreen.js';
+import ContactList from './ContactList/ContactList';
 import './LogIn.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <LoginScreen />
+    <ContactList />
   </React.StrictMode>
 );
 
