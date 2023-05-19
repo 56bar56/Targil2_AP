8b14a74c5778ad2c10bb058182d41cba87edf143
ofekBinder <ofekBinder@gmail.com>
2023-05-19 18:37:57 +0300

    contact chets

--- our_app/src/LoginScreen/LoginScreen.js
+++ our_app/src/LoginScreen/LoginScreen.js
@@ -1,29 +1,34 @@
+import { useRef } from "react";
+import { useState } from "react";
 function LoginScreen() {
-
-    const usernameInput = document.getElementById('usernameInput');
-    const passwordInput = document.getElementById('passwordInput');
-    const messageLoginF = document.getElementById('messageLoginF');
+    const usernameInput = useRef(null);
+    const passwordInput = useRef(null);
+    let [messageLoginF,setmessageLoginF] = useState('');
 
     const loginDeatales = [
         { username: "ofek", password: "12", profilename: "fati", email: "fati@gmail.com", img: "img1" },
         { username: "bar", password: "123", profilename: "haaland", email: "haaland@gmail.com", img: "img2" },
         { username: "ariel", password: "1234", profilename: "lewandowski", email: "lewandowski@gmail.com", img: "img3" }
     ];
-    document.getElementById('loginButton').addEventListener('click', function() {
-        const username = usernameInput.value;
-        const password = passwordInput.value;
+     function logIn () {
+        console.log("im herree");
+        const username = usernameInput.current.value;
+        const password = passwordInput.current.value;
         let weDontMove=1;
         for (let i = 0; i < loginDeatales.length; i++) { //checking if the user and password exist
             if (username === loginDeatales[i].username && password === loginDeatales[i].password) {
                 weDontMove = 0;
                 i=loginDeatales.length;
-                location.href='Chats.html';
+               // location.href='Chats.html';
             } 
         }
         if(weDontMove) { //In case it is not exist, prints error message
-            messageLoginF.textContent = 'wrong password or username';
+            passwordInput.current.value = '';
+            usernameInput.current.value = '';
+            console.log("im herree2");
+            setmessageLoginF('wrong password or username');
         }
-    });
+    }
 
 
 
@@ -35,14 +40,14 @@ function LoginScreen() {
     <div className="col-md-6 offset-md-3 login-form">
       <div className="form-group">
         <label>Username</label>
-        <input id="usernameInput" className="form-control" placeholder="Username" />
+        <input className="form-control" ref = {usernameInput} placeholder="Username" />
       </div>
       <div className="form-group">
         <label>Password</label>
-        <input id="passwordInput" type="password" className="form-control" placeholder="Password" />
+        <input type="password" ref = {passwordInput} className="form-control" placeholder="Password" />
       </div>
       <br />
-      <button type="submit" id="loginButton" className="btn btn-primary btn-block btn-wide">
+      <button onClick={logIn}  className="btn btn-primary btn-block btn-wide" >
         Log In
       </button>
       <br />
@@ -55,7 +60,7 @@ function LoginScreen() {
     </div>
   </div>
 </div>
-<div className="message" id="messageLoginF"></div>
+<div className="message">{messageLoginF}</div>
 </div>
     );
 }
