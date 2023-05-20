6e5ed70aca8dfe8c5e5e6ad5a6bf462e171a2741
56bar56 <barbar56652>
2023-05-20 03:19:57 +0300

    improving the react application.

--- our_app/src/index.js
+++ our_app/src/index.js
@@ -17,7 +17,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChatSection />
+    <RegisterScreen/>
   </React.StrictMode>
 );
 
