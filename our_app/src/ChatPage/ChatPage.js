db363e78400ccdc3286f2da190d8b9b063be5181
ofekBinder <ofekBinder@gmail.com>
2023-05-21 13:21:46 +0300

    fix mistake

--- our_app/src/ChatPage/ChatPage.js
+++ our_app/src/ChatPage/ChatPage.js
@@ -10,10 +10,7 @@ function ChatPage(props) {
     const [chatState, setchatState]= useState(-1);
     const [nameTop, setnameTop]= useState('');
     const [chatsUsers,setchatsUsers]= useState([]);
-    "https://static01.nyt.com/images/2022/10/30/multimedia/30onsoccer-hattracik--1-1cef/30onsoccer-hattracik--1-1cef-mediumSquareAt3X.jpg"
-
-    const [partnerImage,setpartnerImage]=useState("https://static01.nyt.com/images/2022/10/30/multimedia/30onsoccer-hattracik--1-1cef/30onsoccer-hattracik--1-1cef-mediumSquareAt3X.jpg"
-    );
+    const [partnerImage,setpartnerImage]=useState("https://static01.nyt.com/images/2022/10/30/multimedia/30onsoccer-hattracik--1-1cef/30onsoccer-hattracik--1-1cef-mediumSquareAt3X.jpg");
     return (
         <div>
         <ChatUp inMyChat={props.inMyChat} myImage={props.myImage}/>
